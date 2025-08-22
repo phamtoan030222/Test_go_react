88b6ede71ce5553479956ff879db5b5aedf73305
phamtoan030222 <phamductoan030222@gmail.com>
2025-08-23 05:09:58 +0700

    dep

--- backend/main.go
+++ backend/main.go
@@ -13,14 +13,23 @@ import (
 func main() {
 	router := gin.Default()
 
+	// Fix cảnh báo proxy (không trust proxy nào)
+	if err := router.SetTrustedProxies(nil); err != nil {
+		log.Fatal(err)
+	}
+
 	// CORS config
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"http://localhost:5173",            // khi dev
-			"https://test-go-react.vercel.app", // frontend trên Vercel
+			"http://localhost:5173",            // cho dev local
+			"https://test-go-react.vercel.app", // cho frontend trên Vercel
 		},
-		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowMethods: []string{
+			"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin", "Content-Type", "Accept", "Authorization",
+		},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
