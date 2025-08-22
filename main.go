84997d7cb0f77190c5562d2c0a9248a74911ffc2
phamtoan030222 <phamductoan030222@gmail.com>
2025-08-23 00:44:36 +0700

    fix-deploy

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
